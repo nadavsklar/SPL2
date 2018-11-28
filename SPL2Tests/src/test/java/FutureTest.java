8b03bb0cc8bd5910475afdff6b074d90f89259ce
tomerwa <tomerwa@post.bgu.ac.il>
2018-11-28 10:23:27 +0200

    Improving Future Test (still bad tests)

--- SPL2Tests/src/test/java/FutureTest.java
+++ SPL2Tests/src/test/java/FutureTest.java
@@ -30,37 +30,78 @@ public class FutureTest {
     @Test
     public void get() {
         Future<String> F = new Future<String>();
-        String result = "";
-        F.resolve(result);
-        assertEquals(F.get(), result);
+        String result1 = "try1";
+        String result2 = "try2";
+        assertEquals(F.get(),result1);
+        assertNotEquals(F.get(), null);
+        F.resolve(result1);
+        assertEquals(F.get(), result1);
+        assertNotEquals(F.get(), null);
+        F.resolve(result2);
+        assertEquals(F.get(), result2);
+        assertNotEquals(F.get(), result1);
+        assertNotEquals(F.get(), null);
+
+        Future<Integer> G = new Future<Integer>();
+        Integer num1 = 0;
+        Integer num2 = 1;
+        assertEquals(G.get(),num1);
+        assertNotEquals(G.get(), null);
+        G.resolve(num1);
+        assertEquals(G.get(), num1);
+        assertNotEquals(G.get(), null);
+        G.resolve(num2);
+        assertEquals(G.get(), num2);
+        assertNotEquals(G.get(), num1);
+        assertNotEquals(G.get(), null);
     }
+
 
     @Test
     public void resolve() {
         Future<String> F = new Future<String>();
-        String result = "";
-        F.resolve(result);
-        assertEquals(F.get(), result);
+        String result1 = "try1";
+        String result2 = "try2";
+        F.resolve(result1);
+        assertEquals(F.get(), result1);
+        F.resolve(result2);
+        assertNotEquals(F.get(), result1);
+        assertEquals(F.get(), result2);
+
+        Future<Integer> G = new Future<Integer>();
+        Integer num1 = 1;
+        Integer num2 = 2;
+        G.resolve(num1);
+        assertEquals(G.get(), num1);
+        G.resolve(num2);
+        assertNotEquals(G.get(), num2);
+        assertEquals(G.get(), num2);
     }
 
     @Test
     public void isDone() {
         Future<String> F = new Future<String>();
         assertEquals(F.isDone(), false);
-        String result = "";
+        String result = "try";
         F.resolve(result);
         assertEquals(F.isDone(), true);
+
+        Future<Integer> G = new Future<Integer>();
+        assertEquals(G.isDone(), false);
+        Integer num = 0;
+        G.resolve(num);
+        assertEquals(G.isDone(), true);
+
     }
 
     @Test
-    public void get1() {
+    public void getTime() {
         Future<String> F = new Future<String>();
         long timeout = 20;
         TimeUnit unit = TimeUnit.SECONDS;
         F.get(timeout, unit);
-        assertEquals(F.get(timeout, unit), "");
-        F.resolve("");
-
+        assertEquals(F.get(timeout, unit), "try");
+        F.resolve("try");
 
     }
 
