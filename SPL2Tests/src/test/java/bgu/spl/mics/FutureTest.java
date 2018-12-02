ed8da2b84af32064d44821d377d23593018759fa
nadavsklar <sklar@post.bgu.ac.il>
2018-12-02 17:35:07 +0200

    sof

--- SPL2Tests/src/test/java/bgu/spl/mics/FutureTest.java
+++ SPL2Tests/src/test/java/bgu/spl/mics/FutureTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FutureTest {
 
