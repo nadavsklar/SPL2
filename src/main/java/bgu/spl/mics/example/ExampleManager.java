d6371ab6619a66a6eb3c5d52a007db7122fda427
tomerwa <tomerwa@post.bgu.ac.il>
2018-12-11 18:59:21 +0200

    A commit to check if I can do a commit

--- src/main/java/bgu/spl/mics/example/ExampleManager.java
+++ src/main/java/bgu/spl/mics/example/ExampleManager.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-
+//I can do commits?//
 public class ExampleManager {
 
     public static void main(String[] args) {
