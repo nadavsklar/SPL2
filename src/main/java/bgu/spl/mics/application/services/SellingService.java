04eed6b76755a18ccdb74dde6b05f1d067c6792c
nadavsklar <sklar@post.bgu.ac.il>
2018-12-14 17:57:49 +0200

    uploading changes

--- src/main/java/bgu/spl/mics/application/services/SellingService.java
+++ src/main/java/bgu/spl/mics/application/services/SellingService.java
@@ -39,8 +39,11 @@ public class SellingService extends MicroService{
             //receipt.setProccesTick(message.getTimeTick());
             System.out.println(getName() + " sending check avalibility book ");
             Future<Integer> price = sendEvent(new CheckAvailabilityBook(message.getBookTitle()));
-
-			if (price.get() >= 0 && message.getCustomer().getAvailableCreditAmount() >= price.get()) {
+            System.out.println(message.getBookTitle());
+            int priceValue = price.get();
+            System.out.println(priceValue);
+            System.out.println(message.getCustomer().getAvailableCreditAmount());
+			if (priceValue >= 0 && message.getCustomer().getAvailableCreditAmount() >= priceValue) {
 			    System.out.println(getName() + " sending taking book ");
                     sendEvent(new TakeBook(message.getBookTitle()));
                     receipt.setBookTitle(message.getBookTitle());
